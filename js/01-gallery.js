1e52067ff6d9a91155f9b088aec056d313ca8c9f
Oleg833 <oleg.kovtun447@gmail.com>
2023-02-08 06:43:28 +0200

    Update 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const divGallery = document.querySelector('.gallery');
+
+const makeGaleryCard = ({ preview, original, description }) => {
+  const gallerylinkEl = document.createElement('a');
+  gallerylinkEl.classList.add('gallery__link');
+  gallerylinkEl.href = original;
+  gallerylinkEl.rel = 'noopener noreferrer nofollow';
+  gallerylinkEl.target = '_blank';
+
+  const imageEl = document.createElement('img');
+  imageEl.classList.add('gallery__image');
+  imageEl.src = preview;
+  imageEl.dataset.source = original;
+  imageEl.alt = description;
+
+  gallerylinkEl.appendChild(imageEl);
+
+  return gallerylinkEl;
+};
+const imageGalery = galleryItems.map(makeGaleryCard);
+divGallery.append(...imageGalery);
 
 console.log(galleryItems);
